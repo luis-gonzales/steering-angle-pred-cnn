545d449d91fc97b1aef6ae60c9254f039aceaa44
Luis R. Gonzales <lr.gonzales55@gmail.com>
2018-09-04 23:36:10 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,5 +2,6 @@ cd data
 gdrive download 1Bek0kmDjtdPNjG7424go05SxSFD3qsAc
 unzip IMG.zip
 rm IMG.zip
+gdrive download 1Hpny1cuWLx2FHDGxihnO_C55HRDKwaQ_
 cd ..
 gdrive download 1U-X4vFGjS9pNz_CKhRhzp881EbHRR90E
