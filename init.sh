bc7deb9b2c34281f0aa7041246be34aa61e925aa
Luis R. Gonzales <lr.gonzales55@gmail.com>
2018-10-09 09:17:54 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,9 +1,7 @@
 cd data
-wget https://s3.amazonaws.com/udacity-sdc/Vehicle_Tracking/vehicles.zip
-unzip vehicles.zip
-rm vehicles.zip
-wget https://s3.amazonaws.com/udacity-sdc/Vehicle_Tracking/non-vehicles.zip
-unzip non-vehicles.zip
-rm non-vehicles.zip
-gdrive download 1vDeLeBa_VjXlcY0n9ov3AJaCCiTYcnKQ
+gdrive download 1Bek0kmDjtdPNjG7424go05SxSFD3qsAc
+unzip IMG.zip
+rm IMG.zip
+gdrive download 1Hpny1cuWLx2FHDGxihnO_C55HRDKwaQ_
 cd ..
+gdrive download 1U-X4vFGjS9pNz_CKhRhzp881EbHRR90E
